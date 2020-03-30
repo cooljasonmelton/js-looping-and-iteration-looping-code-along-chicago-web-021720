edb3c51906d89a0383c49593b083e4dae978b6a6
cooljasonmelton <cooljasonmelton>
2020-03-30 13:44:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, event){
+    let message_arr = []
+
+    for (let i = 0; i < array.length; i++) {
+        let message = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+        message_arr.push(message)
+    }
+    return message_arr
+}
+
+function countDown(num) {
+    while (num > -1) {
+        console.log(num--)
+    } 
+}
